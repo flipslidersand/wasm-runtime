573b7f82d217f3d4cdc22b23d45983480ad86554
flipslidersand <yukihanashopping0212@gmail.com>
2026-08-23 04:41:51 +0900

    style: fix rustfmt order in prelude.rs

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,5 @@
 pub use crate::module::{parse_module, Module, ValidationError};
-pub use crate::parser::{ParseError, SectionHeader, parse_header, section_iter};
+pub use crate::parser::{parse_header, section_iter, ParseError, SectionHeader};
 pub use crate::sections::{
     ConstExpr, CustomSection, DataMode, DataSegment, ElementInit, ElementMode, ElementSegment,
     Export, ExportKind, FuncBody, FuncType, Global, GlobalType, Import, ImportDesc, Limits,
